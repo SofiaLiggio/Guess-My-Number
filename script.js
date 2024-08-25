ed0b23d12332e2a9fef856e23e40d25ecb553f2d
sofia_liggio <sofialiggio994@gmail.com>
2024-08-25 19:33:39 +0200

    ended script

--- script.js
+++ script.js
@@ -11,19 +11,41 @@ const displayMessage = function (message) {
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
+
+  if (!guess) {
+    displayMessage("⛔️ No Number!");
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
 });
 
-if (!guess) {
-  displayMessage("⛔️ No Number!");
-} else if (guess === secretNumber) {
-  displayMessage("🎉 Correct Number!");
-  document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector("body").style.backgroundColor = "#60b347";
-  document.querySelector(".number").style.width = "30rem";
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
 
-  if (score > highscore) {
-    highscore = score;
-    document.querySelector(".highscore").textContent = highscore;
-  }
-}
+  document.querySelector(".body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
